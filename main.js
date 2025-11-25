414ce605e6080de5286d01adbda5694bb6a2b4ee
Константин-1122 <hamskijham331@gmail.com>
2025-11-25 20:01:50 +0300

    result-1

---
+++ main.js
@@ -0,0 +1,13 @@
+alert('Homework_1');
+
+let UserName ="Victor";
+console.log(UserName);
+let age = "18";
+console.log(age);
+
+UserName = "Konstantin";
+console.log(UserName);
+age = "25";
+console.log(age);
+
+
